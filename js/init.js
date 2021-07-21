6d8c47da92688222438494c385dfc0f6bc39e2d6
JuanCarlosDWP <87781582+JuanCarlosDWP@users.noreply.github.com>
2021-07-21 15:43:10 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({   
+    apiKey: "AIzaSyDk__Am3ZeyMCphqLH3Z9L6m3_c_QBi8B0",
+    authDomain: "chat801jcls.firebaseapp.com",
+    projectId: "chat801jcls",
+    storageBucket: "chat801jcls.appspot.com",
+    messagingSenderId: "973879589827",
+    appId: "1:973879589827:web:224102d0a5f786805edb76",
+    measurementId: "G-KB7H0YHD9M"
+  });
